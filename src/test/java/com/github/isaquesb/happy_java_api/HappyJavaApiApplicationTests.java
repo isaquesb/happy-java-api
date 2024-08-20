b32d8860cb1b29364890478f939e4cb0542a8256
Isaque de Souza <isaquesb@gmail.com>
2024-08-20 00:17:19 -0300

    feat: added starter generated structure

---
+++ src/test/java/com/github/isaquesb/happy_java_api/HappyJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.isaquesb.happy_java_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
